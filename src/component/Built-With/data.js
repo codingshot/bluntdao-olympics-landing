ab640444ab7d09bff073a7df83aa97e1336a009c
codingshot <45281667+codingshot@users.noreply.github.com>
2024-11-05 09:12:32 -0800

    added stephanie as judge to website

--- src/component/Built-With/data.js
+++ src/component/Built-With/data.js
@@ -7,12 +7,21 @@ const technologies = [
     company: "PizzaDAO",
   },
   {
+    img: "https://pbs.twimg.com/profile_images/1788332816591118336/RyX5UCRQ_400x400.jpg",
+    url: "https://x.com/izquieterzzz",
+    name: "Stephanie Izquieta",
+    title: "Events",
+    company: "NEAR Foundation",
+  },
+  {
     img: "https://pbs.twimg.com/profile_images/1654266646809255936/7qaS6N5-_400x400.jpg",
     url: "https://x.com/plugrel",
     name: "PlugRel",
     title: "Super OG Validator",
     company: "BluntDAO",
   },
+
+
 
 
 
